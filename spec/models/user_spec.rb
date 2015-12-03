2d1a0f77bf59a0367c51d618692f9cb7462307e7
dinkengraven <katelyn.dinkgrave@gmail.com>
2015-12-03 12:26:04 -0600

    passes tests for user first/last name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { User.new(first_name: "Letha", last_name: "Rutherford") }
+
+  it "has a first name" do
+    expect(user.first_name).to eq("Letha")
+  end
+
+  it "has a last name" do
+    expect(user.last_name).to eq("Rutherford")
+  end
 end
