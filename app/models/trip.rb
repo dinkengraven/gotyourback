5103bab586de4f8fbce4d0fc84b24f147c1e8160
dinkengraven <katelyn.dinkgrave@gmail.com>
2015-12-14 13:08:13 -0500

    passes tests for location, duration_in_days, route

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,4 @@
 class Trip < ActiveRecord::Base
   # belongs_to :creator, class_name: "User", foreign_key: :creator_id
-  validates :location, presence: true
+  validates :location, :duration_in_days, presence: true
 end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Trip, type: :model do
-  let!(:trip) { Trip.new(location: "Big South Fork, TN") }
+  let!(:trip) { Trip.new(location: "Big South Fork, TN", duration_in_days: 7, route: "Day 1: Point A to Point B, Day 2: Point B to Point C, Day 3: Point C to Point D, etc.") }
 
   it "has a location" do
     expect(trip.location).to eq("Big South Fork, TN")
+  end
+
+  it "has a duration specified in number of days" do
+    expect(trip.duration_in_days).to eq(7)
+  end
+
+  it "has a route" do
+    expect(trip.route).to eq("Day 1: Point A to Point B, Day 2: Point B to Point C, Day 3: Point C to Point D, etc.")
   end
 end
