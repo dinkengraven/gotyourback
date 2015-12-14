a74dcda256c20766abecdf210422398d06583026
dinkengraven <katelyn.dinkgrave@gmail.com>
2015-12-14 13:04:05 -0500

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151203181436) do
+ActiveRecord::Schema.define(version: 20151211194537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "username"
-    t.string   "email"
+  create_table "trips", force: :cascade do |t|
+    t.integer  "user_id"
     t.string   "location"
-    t.string   "password_digest"
+    t.integer  "duration_in_days"
+    t.text     "route"
+    t.text     "details"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
+
+  add_index "trips", ["user_id"], name: "index_trips_on_user_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name",      null: false
+    t.string   "last_name",       null: false
+    t.string   "username",        null: false
+    t.string   "email",           null: false
+    t.string   "location",        null: false
+    t.string   "password_digest", null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "trips", "users"
 end
