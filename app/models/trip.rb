b7c1ea62718de24876f353cd8888021c405a2ee1
dinkengraven <katelyn.dinkgrave@gmail.com>
2015-12-14 14:32:37 -0500

    passes validations tests on location, duration, route

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,4 @@
 class Trip < ActiveRecord::Base
   # belongs_to :creator, class_name: "User", foreign_key: :creator_id
-  validates :location, :duration_in_days, presence: true
+  validates :location, :duration_in_days, :route, presence: true
 end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Trip, type: :model do
-  let!(:trip) { Trip.new(location: "Big South Fork, TN", duration_in_days: 7, route: "Day 1: Point A to Point B, Day 2: Point B to Point C, Day 3: Point C to Point D, etc.") }
+  let!(:trip) { Trip.new(location: "Big South Fork, TN", duration_in_days: 7, route: "Day 1: Point A to Point B, Day 2: Point B to Point C, Day 3: Point C to Point D, etc.", details: "If the weather cooperates, plan on sleeping under the big tarp! It weighs less than each of us carrying a tent.") }
 
   it "has a location" do
     expect(trip.location).to eq("Big South Fork, TN")
@@ -13,5 +13,33 @@ RSpec.describe Trip, type: :model do
 
   it "has a route" do
     expect(trip.route).to eq("Day 1: Point A to Point B, Day 2: Point B to Point C, Day 3: Point C to Point D, etc.")
+  end
+
+  it "has details" do
+    expect(trip.details).to eq("If the weather cooperates, plan on sleeping under the big tarp! It weighs less than each of us carrying a tent.")
+  end
+
+  describe "validations" do
+    it "is invalid without a location" do
+      trip.location = nil
+      trip.save
+      expect(trip.errors.full_messages).to eq(["Location can't be blank"])
+    end
+
+    it "is invalid without a duration" do
+      trip.duration_in_days = nil
+      trip.save
+      expect(trip.errors.full_messages).to eq(["Duration in days can't be blank"])
+    end
+
+    it "is invalid without a route" do
+      trip.route = nil
+      trip.save
+      expect(trip.errors.full_messages).to eq(["Route can't be blank"])
+    end
+  end
+
+  describe "associations" do
+    
   end
 end
