07f3bd1c2dd886d62d7b9bed9db9cc8dc91105c6
dinkengraven <katelyn.dinkgrave@gmail.com>
2015-12-18 15:05:52 -0600

    remove p command

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email])
-    p params
     if user && user.authenticate(params[:session][:password])
       redirect_to "/"
     else
