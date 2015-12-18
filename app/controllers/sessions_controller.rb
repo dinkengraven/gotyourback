a044bb9a925aa6d91bf44b568df914779f1adfcc
dinkengraven <katelyn.dinkgrave@gmail.com>
2015-12-18 15:54:20 -0600

    add log_in method to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
+      log_in(user)
       redirect_to "/"
     else
       flash.now[:error] = "Your credentials did not match. Please try again."
