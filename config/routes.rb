8f31847cece4c8cbca9c810d3f0d5ecc85474988
dinkengraven <katelyn.dinkgrave@gmail.com>
2015-12-17 20:41:31 -0600

    add login screen

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   get "/about" => "pages#about"
 
+  get "/login" => "sessions#new"
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
