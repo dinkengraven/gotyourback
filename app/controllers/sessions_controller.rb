8f11fa1564a460da8edc13dd93424b56e1a48415
dinkengraven <katelyn.dinkgrave@gmail.com>
2015-12-18 15:04:44 -0600

    fix login controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email])
+    p params
+    if user && user.authenticate(params[:session][:password])
+      redirect_to "/"
+    else
+      flash.now[:error] = "Your credentials did not match. Please try again."
+      render "new"
+    end
   end
 end
